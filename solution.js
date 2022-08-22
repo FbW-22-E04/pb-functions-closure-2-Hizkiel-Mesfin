c1e17df924526e4dade9b551ea01c0fc62cd60f1
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-08-22 15:33:37 +0200

    done

---
+++ solution.js
@@ -0,0 +1,14 @@
+//#1
+
+//It will return undefined and 2. The reason why it results in undefined is because the function doesn't return anything but it log the return value of the function inside it. And that's exactly why we recieve 2 and undefined.
+
+//#2
+
+// It result is 5
+
+//#3
+
+//  console.log(obj.prop.getFullname()) this will return 'Aurelio De Rosa' because it's being directly called. Nut when you store a method in a varibale it will yield someother thing after being called by the identifier itself
+
+//#4
+// An error
